c07006e10dcaf6c82e81f5787763028d8367c147
Kacper <kacper.jerzy.kawala@gmail.com>
2022-06-19 15:27:10 +0200

    beta v0.0.2

--- app/src/main/java/pl/lanku/inventory/data/bbscaner/ProductsBBScannerActivity.kt
+++ app/src/main/java/pl/lanku/inventory/data/bbscaner/ProductsBBScannerActivity.kt
@@ -1,30 +1,7 @@
 package pl.lanku.inventory.data.bbscaner
 
-import android.view.View
-import android.widget.EditText
-import android.widget.Toast
 import androidx.activity.ComponentActivity
-import com.journeyapps.barcodescanner.ScanContract
-import com.journeyapps.barcodescanner.ScanIntentResult
-import com.journeyapps.barcodescanner.ScanOptions
-import pl.lanku.inventory.R
-
 
 class ProductsBBScannerActivity(contentLayoutId: Int) : ComponentActivity(contentLayoutId) {
 
-    private val barcodeLauncher = registerForActivityResult(
-        ScanContract()
-    ) { result: ScanIntentResult ->
-        if (result.contents == null) {
-            Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
-        } else {
-            findViewById<EditText>(R.id.name)?.let {
-                it.text = String.format(result.contents)
-            }
-        }
-    }
-
-    fun onScanner(view: View?) {
-        barcodeLauncher.launch(ScanOptions())
-    }
 }
