4de6f41f82cece395a1ff00fda9069417b8296e1
Kacper <kacper.jerzy.kawala@gmail.com>
2022-06-19 17:33:04 +0200

    beta v0.0.3

--- app/src/main/java/pl/lanku/inventory/data/bbscaner/ProductsBBScannerActivity.kt
+++
@@ -1,7 +0,0 @@
-package pl.lanku.inventory.data.bbscaner
-
-import androidx.activity.ComponentActivity
-
-class ProductsBBScannerActivity(contentLayoutId: Int) : ComponentActivity(contentLayoutId) {
-
-}
