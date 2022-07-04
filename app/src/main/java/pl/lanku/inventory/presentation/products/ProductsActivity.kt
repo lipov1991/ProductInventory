ef25e3cdd9d2e53156ad1177b1c69ea6a4de4509
Kacper <kacper.jerzy.kawala@gmail.com>
2022-07-04 19:13:57 +0200

    gamma v0.14

--- app/src/main/java/pl/lanku/inventory/presentation/products/ProductsActivity.kt
+++ app/src/main/java/pl/lanku/inventory/presentation/products/ProductsActivity.kt
@@ -1,14 +1,9 @@
-@file:Suppress("ASSIGNED_BUT_NEVER_ACCESSED_VARIABLE")
-
 package pl.lanku.inventory.presentation.products
 
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -24,13 +19,28 @@ import pl.lanku.inventory.databinding.ActivityProductsBinding
 import pl.lanku.inventory.presentation.productadapter.ProductAdapter
 
 
-open class ProductsActivity : AppCompatActivity() {
+class ProductsActivity : AppCompatActivity() {
     private val viewModel: ProductsViewModel by viewModel()
-    private var barcodeContent: String = ""
-    private var nameDC: String = ""
-    private var descriptionDC: String = ""
-    private var categoryDC: String = ""
     private lateinit var binding: ActivityProductsBinding
+
+    //Value
+    private var barcodeContent: String = ""
+    private var nameContent: String = ""
+    private var descriptionContent: String = ""
+    private var categoryContent: String = ""
+
+    //ET = EditText
+    private lateinit var nameET:TextView //= findViewById<EditText>(R.id.name)
+    private lateinit var descriptionET: TextView //= findViewById<EditText>(R.id.description)
+    private lateinit var categoryET: TextView //= findViewById<EditText>(R.id.category)
+    private lateinit var saveButton: FloatingActionButton //= findViewById<EditText>(R.id.save_product_button)
+
+    //RI = Recycle Item
+    private lateinit var eanRI: TextView //= findViewById<EditText>(R.id.recycler_ean)
+    private lateinit var nameRI: TextView //= findViewById<EditText>(R.id.recycler_name)
+    private lateinit var descriptionRI:TextView //= findViewById<EditText>(R.id.recycler_description)
+    private lateinit var categoryRI: TextView //= findViewById<EditText>(R.id.recycler_category)
+
     private val barcodeLauncher =
         registerForActivityResult(ScanContract()) { result: ScanIntentResult ->
             if (result.contents.isNullOrBlank()) {
@@ -44,8 +54,8 @@ open class ProductsActivity : AppCompatActivity() {
         }
 
     private val formFiledValueChangeListener = object : TextWatcher {
-        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
         override fun afterTextChanged(editable: Editable) {
             validateForm()
         }
@@ -63,6 +73,7 @@ open class ProductsActivity : AppCompatActivity() {
 
     private fun barcodeCheck() {
         viewModel.selectOneItem(barcodeContent).observe(::getLifecycle) { product ->
+//            nameET.setText(product.name)
             findViewById<EditText>(R.id.name).setText(product.name)
             findViewById<EditText>(R.id.description).setText(product.description)
             findViewById<EditText>(R.id.category).setText(product.category)
@@ -80,25 +91,24 @@ open class ProductsActivity : AppCompatActivity() {
     }
 
     private fun validateForm() {
-        nameDC = findViewById<EditText>(R.id.name).text.toString()
-        descriptionDC = findViewById<EditText>(R.id.description).text.toString()
-        categoryDC = findViewById<EditText>(R.id.category).text.toString()
+        nameContent = findViewById<EditText>(R.id.name).text.toString()
+        descriptionContent = findViewById<EditText>(R.id.description).text.toString()
+        categoryContent = findViewById<EditText>(R.id.category).text.toString()
         findViewById<FloatingActionButton>(save_product_button)?.isEnabled =
-            barcodeContent.isNotBlank() && nameDC.isNotBlank() && descriptionDC.isNotBlank() && categoryDC.isNotBlank()
+            barcodeContent.isNotBlank() && nameContent.isNotBlank() && descriptionContent.isNotBlank() && categoryContent.isNotBlank()
     }
 
     private fun setFormFieldsEnabled(enable: Boolean) {
         findViewById<EditText>(R.id.name).isEnabled = enable
         findViewById<EditText>(R.id.description).isEnabled = enable
         findViewById<EditText>(R.id.category).isEnabled = enable
-        findViewById<FloatingActionButton>(R.id.save_product_button).isEnabled = enable
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        val nameET = findViewById<EditText>(R.id.name)
-//        val descriptionET = findViewById<EditText>(R.id.description)          NIE DZIAUA!!!!!!!!
-//        val categoryET = findViewById<EditText>(R.id.category)                JEBANE GÓWNO
+//        nameET = findViewById<EditText>(R.id.name)
+//        descriptionET = findViewById<EditText>(R.id.description)
+//        categoryET = findViewById<EditText>(R.id.category)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_products)
 
         val recyclerViewProducts = binding.recycler
@@ -107,7 +117,7 @@ open class ProductsActivity : AppCompatActivity() {
 
         productAdapter.setOnClickItemListener(object : ProductAdapter.OnItemClickListener{
             override fun onItemClick(position: Int) {
-                Toast.makeText(this@ProductsActivity,"Wybrano produkt $position",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@ProductsActivity,"Wybrano produkt",Toast.LENGTH_SHORT).show()
                 layoutManager.findViewByPosition(position).let {
                     if (it != null) {
                         barcodeContent = it.findViewById<TextView>(R.id.recycler_ean).text.toString()
@@ -115,6 +125,7 @@ open class ProductsActivity : AppCompatActivity() {
                         findViewById<EditText>(R.id.description).setText(it.findViewById<TextView>(R.id.recycler_description).text.toString())
                         findViewById<EditText>(R.id.category).setText(it.findViewById<TextView>(R.id.recycler_category).text.toString())
                         setFormFieldsEnabled(true)
+                        validateForm()
                     }
                 }
             }
@@ -135,7 +146,7 @@ open class ProductsActivity : AppCompatActivity() {
         }
 
         findViewById<FloatingActionButton>(save_product_button).setOnClickListener {
-            viewModel.save(Product(barcodeContent, nameDC, descriptionDC, categoryDC))
+            viewModel.save(Product(barcodeContent, nameContent, descriptionContent, categoryContent))
             findViewById<EditText>(R.id.name).text.clear()
             findViewById<EditText>(R.id.description).text.clear()
             findViewById<EditText>(R.id.category).text.clear()
